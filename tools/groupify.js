3d9a08f4aea8267b704b47bf31fdd15a9c102bd0
Andrea Del Signore <sejerpz@gmail.com>
2025-09-17 13:15:43 +0200

    groupify: add serialization support

--- tools/groupify.js
+++ tools/groupify.js
@@ -11,7 +11,9 @@ function run() {
         setup() {
             let plugins = ref([])
             let selected_plugin = ref(null)
-            let selected_ttl = ref('')
+            let ttl_preview = ref('')
+            let original_ttl = ''
+            let patched_ttl = ''
             let err = ref(null)
             let ports = ref([])
             const groups = ref([])
@@ -104,7 +106,7 @@ function run() {
                         console.log('downloading ', plugin, pluginTtl)
                         // get the folder list
 
-                        selected_ttl.value = ''
+                        ttl_preview.value = original_ttl = ''
                         ports.value = []
 
                         axios.get(plugin.data.url)
@@ -116,9 +118,9 @@ function run() {
                                         // found the ttl
                                         axios.get(item.url)
                                         .then(res => {
-                                            selected_ttl.value = atob(res.data.content)
+                                            ttl_preview.value = original_ttl = atob(res.data.content)
                                             // parse the ports
-                                            const quads = parser.parse(selected_ttl.value)
+                                            const quads = parser.parse(original_ttl )
                                             let subjects = new Set()
 
                                             // distinct subject
@@ -161,7 +163,7 @@ function run() {
 
                                                     const port = { id: id,
                                                                    label: label?.object?.id.replace('"', '').replace('"', ''),
-                                                                   symbol: symbol?.object.id,
+                                                                   symbol: symbol?.object.id.replace('"', '').replace('"', ''),
                                                                    index: parseInt(index.object.value) ?? 0,
                                                                    group: -1,
                                                                    selected: false
@@ -233,41 +235,194 @@ function run() {
 
             function serialize()
             {
-                try {
-                    const parser = new N3.Parser()
-                    const quads = []
-                    const prefixes = []
-                    const comments = []
+                // the patching is manual, can't reuse N3 too complex
+                const lines = original_ttl.split('\n')
 
-                    parser.parse(selected_ttl.value, 
-                        {
-                            // onQuad (required) accepts a listener of type (quad: RDF.Quad) => void
-                            onQuad: (err, quad) => { quads.push(quad); },
-                            // onPrefix (optional) accepts a listener of type (prefix: string, iri: NamedNode) => void
-                            onPrefix: (prefix, iri) => { prefixes.push(prefix); },
-                            // onComment (optional) accepts a listener of type (comment: string) => void
-                            onComment: (comment) => { comments.push(comment); },
+                const updateLineValue = (lines, lineIndex, name, newValue) => {
+                    const toks = lines[lineIndex].split(' ')
+                    let newLine = ""
+                    let skipNext = false
+
+                    for(const tok of toks) {
+                        if (tok == '') {
+                            newLine += ' '
+                            continue;
                         }
-                    )
-                    const writer = new N3.Writer({ format: 'N-Triples', prefixes: prefixes });
 
-                    prefixes.forEach(prefix => {
-                        writer.addPre
-                    })
-                    quads.forEach(element => {
-                        writer.addQuad(quads)
-                    });
-                    for(let port in ports.value) {
-                        writer.addQuad(new  N3.Quad(
-                            port.id,                                       // Subject
-                            new N3.NamedNode('http://example.org/cartoons#name'), // Predicate
-                            port.group                                     // Object
-                        ));
+                        if (skipNext) {
+                            if (tok.endsWith(';')) // preserve the line ending even if we've skipped the value
+                                newLine += ';'
+                            skipNext = false
+                        } else {
+                            newLine += tok
+                            if (tok.indexOf(':' + name) >= 0) {
+                                newLine += ' ' + newValue
+                                skipNext = true
+                            }
+                        }
                     }
-                    writer.end((error, result) => console.log(error, result));
-                } catch(err) {
-                    console.error('error serializing ', err)
+
+                    return newLine
                 }
+                const patchPort = (patchInfo) => {
+                    const port = patchInfo.port
+                    const lines = patchInfo.lines
+                    const indexLineIndex = patchInfo.indexLineIndex
+                    const groupLineIndex = patchInfo.groupLineIndex
+                    const symbolLineIndex = patchInfo.symbolLineIndex
+                    const symbolPrefix = patchInfo.symbolPrefix
+                    const indentation = patchInfo.symbolIndentation ?? '    '
+                    const group = groups.value.find(item => item.id == port.group)
+
+                    // ok new port found, add the config to the previous
+                    if (indexLineIndex >= 0) {
+                        // fix the index
+                        lines[indexLineIndex] = updateLineValue(lines, indexLineIndex, 'index', port.index)
+                    } else {
+                        lines.splice(symbolLineIndex, 0, indentation + symbolPrefix + ':index ' + port.index.toString() + ';')
+                    }
+                    if (group.id == -1) {
+                        // remove group
+                        if (groupLineIndex >= 0)
+                            lines.splice(groupLineIndex, 1)
+                    } else {
+
+                        if (groupLineIndex >= 0) {
+                            // fix the index
+                            lines[groupLineIndex] = updateLineValue(lines, groupLineIndex, 'group', group.name)
+                        } else {
+                            lines.splice(symbolLineIndex, 0, indentation + 'pg:group ' + group.name + ' ;')
+                        }
+                    }
+                }
+
+                let port = undefined
+                let indexLineIndex = -1
+                let symbolLineIndex = -1
+                let symbolPrefix = ""
+                let symbolIndentation = '    '
+                let groupLineIndex = -1
+                let lastPrefixIndex = -1
+                let pluginId = null
+                const usedGroupsId = []
+
+                for(port of ports.value) {
+                    let insidePort = 0
+
+                    indexLineIndex = -1
+                    symbolLineIndex = -1
+                    symbolPrefix = ""
+                    symbolIndentation = '    '
+                    groupLineIndex = -1
+                    lastPrefixIndex = -1
+
+                    if (port.group >= 0 && !usedGroupsId.includes(port.group))
+                        usedGroupsId.push(port.group)
+
+                    for(let index = 0;index < lines.length; index++) {
+                        const line = lines[index]
+
+                        // search the plugin id (can't be on the first row)
+                        if (pluginId == null && index > 0 && line.indexOf(':Plugin') >= 0)
+                            pluginId = lines[index-1]
+                        if (lastPrefixIndex == -1 && line.indexOf('@prefix ') >= 0)
+                            lastPrefixIndex = index
+
+                        if (line.indexOf(':port') >= 0) {
+                            // ok new port found, add the config to the previous
+                            if (symbolLineIndex >= 0) {
+                                patchPort({
+                                    port: port,
+                                    lines: lines,
+                                    indexLineIndex: indexLineIndex,
+                                    groupLineIndex: groupLineIndex,
+                                    symbolLineIndex: symbolLineIndex,
+                                    symbolPrefix: symbolPrefix,
+                                    symbolIndentation: symbolIndentation
+                                })
+                            }
+                            insidePort = 0 // start of port descriptor
+                            indexLineIndex = -1
+                            symbolLineIndex = -1
+                            symbolPrefix = ""
+                            groupLineIndex = -1
+                        }
+
+                        if (insidePort < 2) { // finding a control input port port
+                            if (line.indexOf(':ControlPort') >= 0)
+                                insidePort++ // we need to find controlport and inputport
+                            if (line.indexOf(':InputPort') >= 0)
+                                insidePort++ // we need to find controlport and inputport
+
+                        } else {
+                            if (line.indexOf(':index') >= 0)
+                                indexLineIndex = index
+                            else if (line.indexOf(':symbol') >= 0)
+                            {
+                                // parse symbol name
+                                let lineSymbolName = line.trim().split(' ')[1]?.replace('"', '').replace('"', '')
+                                
+                                console.log('line symbol: ', lineSymbolName)
+                                if (lineSymbolName == port.symbol) {
+                                    symbolLineIndex = index
+                                    symbolPrefix = line.split(':')[0]?.trim() ?? ""
+                                    symbolIndentation = line.replace(line.trim(), '')
+                                }
+                            } else if (line.indexOf(':group') >= 0) {
+                                groupLineIndex = index
+                            }
+
+                        }
+                    }
+                }
+
+                // if indexSymbolLine >= 0 we are handling the last port
+                if (symbolLineIndex >= 0) {
+                    patchPort({
+                        port: port,
+                        lines: lines,
+                        indexLineIndex: indexLineIndex,
+                        groupLineIndex: groupLineIndex,
+                        symbolLineIndex: symbolLineIndex,
+                        symbolPrefix: symbolPrefix,
+                        symbolIndentation: symbolIndentation
+                    })
+                }
+
+
+                // insert group extension prefix
+                lastPrefixIndex++
+                lines.splice(lastPrefixIndex, 0, '@prefix pg: <http://lv2plug.in/ns/ext/port-groups#> .')
+
+                // add the groups
+                if (usedGroupsId.length > 0) {
+                    lines.push('')
+
+                    for(let groupId of usedGroupsId) {
+                        const group = groups.value.find(item => item.id == groupId)
+
+                        if (group) {
+                            lines.push(`${pluginId}:${group.name}`)
+                            lines.push(`    a pg:InputGroup ;`)
+                            lines.push(`    pg:symbol "${group.name}" ;`)
+                            lines.push(`    pg:name "${group.label}" .`)
+                        }
+                    }
+                }
+                // join the lines
+                patched_ttl = lines.join('\n')
+                ttl_preview.value = patched_ttl
+            }
+
+            function switchPreview(preview) {
+                console.log('switch preview ', preview)
+                if (preview == 'patched')
+                    ttl_preview.value = patched_ttl
+                else if (preview == 'diff') {
+                    ttl_preview.value = diff.createTwoFilesPatch("original", "new", original_ttl, patched_ttl)
+                }
+                else
+                    ttl_preview.value = original_ttl
             }
 
             onMounted(() => {
@@ -290,15 +445,15 @@ function run() {
             })
 
             // initalize groups
-            groups.value.push({id: -1, label: 'none', color: "white"})
+            groups.value.push({id: -1, label: 'none', name: '<#none#>', color: "white"})
             for(let i=0; i<32; i++) {
-                groups.value.push({id: i, label: (i+1).toString(), color: "red"})
+                groups.value.push({id: i, label: (i+1).toString(), name: 'GROUP_' + (i+1).toString(), color: "red"})
             }
             return {
                 selected_plugin,
                 plugins,
                 err,
-                selected_ttl,
+                ttl_preview,
                 ports,
                 groups,
                 el,
@@ -307,7 +462,8 @@ function run() {
                 get_plugin_list,
                 select_plugin,
                 on_port_dropped,
-                serialize
+                serialize,
+                switchPreview
             }
         }
     })
